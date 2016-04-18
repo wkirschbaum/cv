c5ec11702448928b7228122d1d0621b7140a0f76
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2016-04-18 22:55:53 +0200

    Add favicon

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'asciidoctor'
+require 'asciidoctor-pdf'
 
 content = File.read('./main.adoc')
 html_content = Asciidoctor.convert content, header_footer: true, safe: :safe
