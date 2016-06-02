9f09f963b372879da2b9bfc2c646b227eb3dbebd
Wilhelm Kirschbaum <wkirschbaum@gmail.com>
2016-06-02 09:54:44 +0200

    Update

--- server.rb
+++ server.rb
@@ -5,6 +5,10 @@ require 'asciidoctor-pdf'
 content = File.read('./main.adoc')
 html_content = Asciidoctor.convert content, header_footer: true, safe: :safe
 
-get '/' do
+get '/wilhelm' do
   html_content
+end
+
+get '/' do
+  "Please request a link."
 end
